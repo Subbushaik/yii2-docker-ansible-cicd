5817b8735b60a55e53a729872619fc605842bd2d
Ubuntu <ubuntu@ip-172-31-80-118.ec2.internal>
2025-06-04 13:49:31 +0000

    application code modified

---
+++ app/contact.php
@@ -0,0 +1,5 @@
+<?php
+echo "<h1>Contact Page</h1>";
+echo "<p>Email us at contact@example.com</p>";
+echo "<a href='/index.php'>Go Back</a>";
+
---
+++ app/index.php
@@ -0,0 +1,5 @@
+<?php
+echo "<h1>Welcome to My Simple PHP App</h1>";
+echo "<p>This is the homepage.</p>";
+echo "<a href='/contact.php'>Contact Us</a>";
+
